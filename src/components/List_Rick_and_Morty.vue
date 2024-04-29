49c5c945489fbd5a0670479ee3ad6068e03efe30
Eduardadb0912 <128000425+Eduardadb0912@users.noreply.github.com>
2024-04-29 20:46:05 -0300

    Update List_Rick_and_Morty.vue

--- src/components/List_Rick_and_Morty.vue
+++ src/components/List_Rick_and_Morty.vue
@@ -3,11 +3,11 @@ const personagem = defineProps(["name", "id", "image", "location", "gender", "st
 </script>
 
 <template> 
-    <div class="col-md-3">
+    <div class="col-md-3 d-flex">
         <div class="card p-3 mb-4">
             <img :src=personagem.image>
             <p class="text-center"><strong>NAME: </strong> {{ personagem.name }}</p>
-            <p class="text-center"><strong>LOCATION: </strong>{{ personagem.location }}</p>
+            <p class="text-center"><strong>LOCAL: </strong>{{ personagem.location }}</p>
             <p class="text-center"><strong>GENDER: </strong>{{ personagem.gender }}</p>
             <p class="text-center"><strong>STATUS: </strong>{{ personagem.status }}</p>
             <p class="text-center"><strong>SPECIES: </strong>{{ personagem.species }}</p>
