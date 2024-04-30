e0f538712b7d53898210b4de0e1cd5cafe5d3683
Eduardadb0912 <128000425+Eduardadb0912@users.noreply.github.com>
2024-04-30 15:07:22 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,11 @@ let personagens = reactive(ref())
 
 onMounted(() => {
   fetch("https://rickandmortyapi.com/api/character/?limit=20&offset=0")
-  .then(response => response.json())
-  .then(response => {
-    personagens.value = response.results
-    console.log(personagens)
-  })
+    .then(response => response.json())
+    .then(response => {
+      personagens.value = response.results
+      console.log(personagens)
+    })
 })
 
 </script>
@@ -18,20 +18,11 @@ onMounted(() => {
 <template>
   <main style="background: linear-gradient(to bottom, #6be0e9, #d9f344)">
     <div class="container">
-            <div class="card-body mt-4 mb-8 row" style="padding: 0;">
-              <ListRick 
-                v-for="personagem in personagens" 
-                :key="personagem.id" 
-                :name="personagem.name"
-                :image="personagem.image"
-                :species="personagem.species"
-                :status="personagem.status"
-                :gender="personagem.gender"
-                :location="personagem.location.name"
-                :episode = "personagem.episode"
-              />
-            </div>
-          </div>
+      <div class="card-body mt-4 mb-8 row" style="padding: 0;">
+        <ListRick v-for="personagem in personagens" :key="personagem.id" :name="personagem.name"
+          :image="personagem.image" :species="personagem.species" :status="personagem.status"
+          :gender="personagem.gender" :location="personagem.location.name" :episode="personagem.episode" />
+      </div>
+    </div>
   </main>
 </template>
-
