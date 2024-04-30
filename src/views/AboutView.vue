83d88512f80a621fd8448fc5b14c286d63d7fd37
Eduardadb0912 <128000425+Eduardadb0912@users.noreply.github.com>
2024-04-30 15:06:55 -0300

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,30 @@
 <template>
 
-    <center>
+  <center>
     <div class="card card_a">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="../assets/Perfil.jpeg" class="card-img" style="width: 28vh" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card_a-body" style="margin-left: 3vh">
-       <h5 class="card-title">Maria Eduarda Del Boni Da Silva</h5>
-       <ul style="margin-top:8.5vh;"></ul>
-       <p> Nasci no dia 09 de Dezembro de 2004 (19 anos), 
-        <br>na cidade de Marilía/SP. 
-        <br> Atualmente estou cursando Análise e Desenvolvimento de Sistemas(ADS) e fazendo estágio na UNIMAR,
-        <br> estou no 3° TERMO - 3/6.</p>
-        <p><b>CONTATO:</b> (14) 9 9860-3187</p>  
-        <p><b>E-MAIL:</b> delboni.unimar@gmail.com</p>
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="../assets/Perfil.jpeg" class="card-img" style="width: 28vh" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card_a-body" style="margin-left: 3vh">
+            <h5 class="card-title">Maria Eduarda Del Boni Da Silva</h5>
+            <ul style="margin-top:8.5vh;"></ul>
+            <p> Nasci no dia 09 de Dezembro de 2004 (19 anos),
+              <br>na cidade de Marilía/SP.
+              <br> Atualmente estou cursando Análise e Desenvolvimento de Sistemas(ADS) e fazendo estágio na UNIMAR,
+              <br> estou no 3° TERMO - 3/6.
+            </p>
+            <p><b>CONTATO:</b> (14) 9 9860-3187</p>
+            <p><b>E-MAIL:</b> delboni.unimar@gmail.com</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-</center>
+  </center>
 
-  
-  
+
+
 </template>
 
 <style>
@@ -31,7 +32,7 @@ body {
   background-color: #6be0e9;
 }
 
-.card_a{
+.card_a {
   width: 680px;
   height: 350px;
   background-color: #ffffff;
@@ -40,5 +41,4 @@ body {
   margin-top: 100px;
   border-radius: 20px;
 }
-
 </style>
